fc5f405f486464c1b26aa80163166343bdc5845b
Jackie <taekwondo.chan@gmail.com>
2020-09-15 22:36:04 -0700

    Made the bot a more agressive salesman

--- chat_bot.py
+++ chat_bot.py
@@ -3,7 +3,7 @@ from random import choice
 
 def get_bot_response(user_response):
     respond_hungry = ["I'm sure I can pick a great burger for you.", "Sounds like you could use a good burger.", "Hunger is best solved with a burger to the facehole."]
-    respond_full = ["You're full? No such thing. You could easily stuff down another small cheeseburger.", "You're lying. You get a burger.", "You're not full. You're just weak. A burger it is."]
+    respond_full = ["You're full? No such thing. You must consume another burger.", "You're lying. You get a burger. No, there are no other options.", "You're not full. You're just weak. A burger it is."]
 
     if user_response_1 == "hungry":
         return choice(respond_hungry)
@@ -14,8 +14,8 @@ def get_bot_response(user_response):
 
 
 def get_bot_response_2(user_response_2):
-    respond_patty = ["That's a lot of meat. Are you sure about this?", "You might get the meat sweats. Is that alright with you?", "For reasons of legality I must confirm you actually want another patty."]
-    respond_other = ["Wrong answer. I'm assuming you mistyped and meant to write 'patty.'", "Close but no cigar. More meat is a better idea.", "That's a strange well to spell 'patty.'"]
+    respond_patty = ["That's a lot of meat. Are you sure about this?", "You might get the meat sweats. Is that alright with you?", "For legal reasons I must confirm you actually want more protein."]
+    respond_other = ["Wrong answer. I'm assuming that was a type and you meant to write 'patty.'", "Close but no cigar. More meat is a better idea.", "That's a strange well to spell 'patty.'"]
 
     if user_response_2 == "patty" or user_response_2 == "meat":
         return choice(respond_patty)
@@ -33,7 +33,7 @@ def get_bot_response_3(user_response_3):
         return choice(respond_other_2)
 
 
-print("Hello, I am here to help you decide on your nutritional needs.")
+print("Hello, xXRealHumanBeingXx here. I am to assist you with your dietary needs.")
 print("Enter 'stop' or 'done' at any time to exit.")
 
 user_response_1 = ""
@@ -44,6 +44,7 @@ while True:
     user_response_1 = str.lower(input("How are you currently feeling? Hungry or full? "))
 
     if user_response_1 == "stop" or user_response_1 == "done":
+        print("Well you win some, you lose some.")
         exit()
     elif user_response_1 == "hungry":
         bot_response = get_bot_response(user_response_1)
@@ -61,9 +62,10 @@ while True:
     
 
 while True:
-    user_response_2 = str.lower(input("Now that we know you're ordering something, what topping do you want on your burger? "))
+    user_response_2 = str.lower(input("So you're getting a burger. But what topping do you want on it? "))
 
     if user_response_2 == "stop" or user_response_2 == "done":
+        print("I didn't want to talk to you anyways.")
         exit()
     elif user_response_2 == "patty" or user_response_2 == "meat":
         bot_response_2 = get_bot_response_2(user_response_2)
@@ -80,6 +82,7 @@ while True:
     user_response_3 = str.lower(input("Yes or no? "))
 
     if user_response_3 == "stop" or user_response_3 == "done":
+        print("What a tease.")
         exit()
     elif user_response_3 == "yes":
         bot_response_3 = get_bot_response_3(user_response_3)
